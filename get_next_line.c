485ff91793d393a445c0a0824585881e40936498
Cameron <cameronlambert98@gmail.com>
2018-06-29 15:44:51 -0700

    update

---
+++ get_next_line.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calamber <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/26 23:47:06 by calamber          #+#    #+#             */
+/*   Updated: 2018/06/29 03:31:24 by calamber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	get_next_line(const int fd, char **line)
+{
+	char	buffer[BUFF + 1];
+
+	if (line[0] || line[1])
+	{
+		free(line[0]);
+		line[0] = ft_strdup(line[1]);
+		free(line[1]);
+		return (readline(fd, 2, line));
+	}
+	else
+	{
+		if (!(readline(fd, 1, line)));
+			return (0);
+		return (readline(fd, 2, line));
+	}
+
+	return (0);
+}
+
+int	readline(const int fd, int p, char **line)
+{
+	
+	return (0);
+}
+
+
