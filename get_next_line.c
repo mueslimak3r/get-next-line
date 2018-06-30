8c19a7628e3a7f38386e7debeb5b9c1ec4130e2a
Cameron Lambert <calamber@e1z3r3p2.42.us.org>
2018-06-29 19:20:52 -0700

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,12 @@
 /*   By: calamber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 23:47:06 by calamber          #+#    #+#             */
-/*   Updated: 2018/06/29 03:31:24 by calamber         ###   ########.fr       */
+/*   Updated: 2018/06/29 17:59:04 by calamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	get_next_line(const int fd, char **line)
 {
-	char	buffer[BUFF + 1];
-
 	if (line[0] || line[1])
 	{
 		free(line[0]);
@@ -27,13 +25,23 @@ int	get_next_line(const int fd, char **line)
 			return (0);
 		return (readline(fd, 2, line));
 	}
-
 	return (0);
 }
 
 int	readline(const int fd, int p, char **line)
 {
-	
+	char	buffer[BUFF + 1]
+	char	temp;
+	int		i;
+
+	i = 1;
+	buffer[BUFF + 1] = '\0';
+	while ((temp != '\n') && (buffer))
+	{
+		read(fd, temp, 1);
+		buffer[i++] = temp;
+	}
+
 	return (0);
 }
 
