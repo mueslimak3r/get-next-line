30e3cea169c760aff56a90c56863e6a5ffd1965d
Cameron Lambert <calamber@e1z2r5p26.42.us.org>
2018-07-03 22:43:57 -0700

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: calamber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/30 19:38:09 by calamber          #+#    #+#             */
-/*   Updated: 2018/06/30 19:57:21 by calamber         ###   ########.fr       */
+/*   Updated: 2018/07/03 22:43:38 by calamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,30 @@
 
 int		get_next_line(const int fd, char **line)
 {
-	static char **buffer;
+	static char *files[4096];
+	char		swap[BUFF_SIZE + 1];
+	char		*swap2;
+	char		*swap3;
 
-	while (buffer[i])
+	if (line)
+		free(line);
+	if (!(buffer[fd]))
 	{
+		line = getnewline(fd, buffer, &files);
+		return (1);
+	}
+	else
+	{
+		if (ft_strchr(files[fd]))
 
+		else
+		{
+			swap2 = ft_strdup(files[fd]);
+			free(files[fd];
+			swap3 = getnewline(fd, buffer, &files);
+			line = ft_strjoin(swap2, swap3);
+			free(swap2);
+			free(swap3);
+		}
 	}
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,26 @@
 /*   By: calamber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/30 19:24:12 by calamber          #+#    #+#             */
-/*   Updated: 2018/06/30 19:57:25 by calamber         ###   ########.fr       */
+/*   Updated: 2018/07/01 19:39:00 by calamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef
-# define
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-typedef static struct	s_fd
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 42
+
+typedef static struct	s_list
 {
 	char				*buffer;
 	const int			fd;
 	struct s_fd			*next;
-}						t_fd
+}						t_list
 
 int					get_next_line(const int fd, char **line);
 
-# endif
+#endif
