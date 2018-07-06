e0bb7e259357a000e07f33d4174f27712dbc536b
Cameron <cameronlambert98@gmail.com>
2018-07-06 03:21:41 -0700

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: calamber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/30 19:38:09 by calamber          #+#    #+#             */
-/*   Updated: 2018/07/05 20:22:49 by calamber         ###   ########.fr       */
+/*   Updated: 2018/07/06 03:21:24 by calamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ int		get_next_line(const int fd, char **line)
 {
 	static char *files[4096];
 	char		*swap2;
-	char		*swap3;
-	int			n;
+	int		n;
 
 	if (line)
 		free(line);
@@ -40,20 +39,20 @@ int		get_next_line(const int fd, char **line)
 		else
 		{
 			swap2 = ft_strdup(files[fd]);
-			free(files[fd];
-			swap3 = getline(fd, files);
-			line = ft_strjoin(swap2, swap3);
+			free(files[fd]);
+			getline(fd, files);
+			line = ft_strjoin(swap2, );
 			free(swap2);
-			free(swap3);
 		}
 	}
+	return (0);
 }
 
-int	getline(const int fd, char **files)
+void	getline(const int fd, char **files)
 {
 	if (!(files[fd] = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
-		return (0);
+		return ;
 	files[fd][BUFF_SIZE + 1] = '\0';
 	read(fd, files[fd], BUFF_SIZE);
-	return (1);
+	return ;
 }
